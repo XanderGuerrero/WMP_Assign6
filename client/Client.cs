d6a7e58f4f9121d0c5c9876cf9359f9781a5ac30
XanderGuerrero <alexander.d.guerrero@outlook.com>
2016-12-12 18:29:55 -0500

    Import Project

---
+++ client/Client.cs
@@ -0,0 +1,394 @@
+﻿//NAME:	        Client.cs
+//PROJECT:      Assignment6
+//DATE:	        04/11/2014
+//AUTHORS:	    Alex Guerrero, Manbir Singh
+//DISCRIPTION:	The Client side program attempts to establish conncetions with a server host by using the IP 
+//              address supplied by the user at the start of the program.  If the program is successful in 
+//              connecting, a write and read thread is created to handle messaging between the two programs.
+//              When the user chooses to exit both the server and client shut down communications
+//              HOW TO RUN:  client cmd line arguments are ip of server and chat name
+//              
+
+
+
+
+//references
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+
+
+
+
+//CLASS
+//-------------------------
+//NAME	:	Client
+//PURPOSE :	This object contains sockets, threads and variables as data members to establish a chat connection over a network
+//-------------------------
+public class Client
+{
+    //class Variables
+    public static Socket sender;
+    public static Thread write;
+    public static Thread read;
+    public static string clientName;
+    public static string serverName;
+    public static List<string> outputArea = new List<string>();
+    static int areaHeights = 0;
+    static bool firstSend = true;
+    static bool firstReceive = true;
+
+
+
+
+    /* ---------------------------
+    *	Name	:   StartClient
+    *
+    *	Purpose :   StartClient tries to establish a conncetion to a host computer
+    *	            by using the port 11000 and the client entered IP address of the host.
+    *	           
+    *
+    *	Inputs	:	NONE
+    *	
+    *	Outputs	:	Program output is placed into a list and the contents of the list are displayed to the
+    *               screen
+    *               
+    *	Returns	:	NONE
+    */
+    public static void StartClient(string ipaddress, string userName)
+    {
+        //place the userName into the client name variable
+        clientName = userName;
+
+        //determine the window height  minus 2
+        areaHeights = (Console.WindowHeight - 2);
+        drawScreen();
+
+        // Data buffer for incoming data.
+        // Connect to a remote device. if an error occurs
+        // catch them
+        try
+        {
+            // Establish the remote endpoint for the socket.
+            // This example uses port 11000 on the local computer.
+            IPHostEntry ipHostInfo = Dns.Resolve(ipaddress);
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+            // Create a TCP/IP  socket.
+            sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // Connect the socket to the remote endpoint. Catch any errors.
+            try
+            {
+                try
+                {
+                    //if connect fails, display error msg to screen in catch
+                    //and exit
+                    sender.Connect(remoteEP);
+                }
+                catch (Exception e)
+                {
+                    outputArea.Insert(0, e.Message);
+                    drawScreen();
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+
+                //connection has been made, inform user and start the write and read threads
+                outputArea.Insert(0, "Connected to the server, enter *esc to disconnect...");
+                drawScreen();
+                write = new Thread(new ThreadStart(WriteToServer));
+                write.Start();
+                read = new Thread(new ThreadStart(ReadFromServer));
+                read.Start();
+            }
+            //display the cmd line argument expection error msg
+            catch (ArgumentNullException ane)
+            {
+                outputArea.Insert(0, ane.ToString());
+                drawScreen();
+            }
+            //display the socket expection error msg
+            catch (SocketException se)
+            {
+                outputArea.Insert(0, se.ToString());
+                drawScreen();
+            }
+            //display the expection error msg
+            catch (Exception e)
+            {
+                outputArea.Insert(0, e.ToString());
+                drawScreen();
+            }
+        }
+        //Display error
+        catch (Exception e)
+        {
+            outputArea.Insert(0, e.ToString());
+            drawScreen();
+        }
+    }
+
+
+
+
+    /* ---------------------------
+    *	Name	:   WriteToServer
+    *
+    *	Purpose :   Writes user entered data from the keyboard into a buffer and sends it to the server.  
+    *	            A first send messages sends the client username.  If the user chooses to exit with 
+    *	            the "*esc" request, the client shuts down and sends notice to the server
+    *
+    *	Inputs	:	NONE
+    *	
+    *	Outputs	:	Program output is placed into a list and the contents of the list are displayed to the
+    *               screen by a call to drawScreen
+    *               
+    *	Returns	:	NONE
+    */
+    public static void WriteToServer()
+    {
+        string data = null;
+        byte[] msg;
+        int bytesSent;
+
+        //if this is the first send,
+        if (firstSend == true)
+        {
+            //place the client name in the data buffer
+            data = clientName;
+
+            // Encode the data string into a byte array.
+            msg = Encoding.ASCII.GetBytes(data);
+
+            // Send the data through the socket.
+            //set firstSend to false to never execute this
+            //section of code
+            bytesSent = sender.Send(msg);
+            firstSend = false;
+            data = null;
+        }
+
+        //constantly write
+        while (true)
+        {
+            //prepare string to send to server
+            data = null;
+            data = clientName + ": ";
+            data += Console.ReadLine();
+
+            try
+            {
+                //add the client entered data to the list and call drawScreen to update the screen
+                outputArea.Insert(0, data);
+                drawScreen();
+
+                //if the user requests the *esc command to quit the program
+                //inform the server and abort the read thread
+                if (data == clientName + ": " + "*esc")
+                {
+                    // Encode the data string into a byte array.
+                    msg = Encoding.ASCII.GetBytes(data);
+                    // Send the data through the socket.
+                    bytesSent = sender.Send(msg);
+                    read.Abort();
+                    data = null;
+                    break;
+                }
+
+                // Encode the data string into a byte array.
+                msg = Encoding.ASCII.GetBytes(data);
+
+                // Send the data through the socket.
+                bytesSent = sender.Send(msg);
+            }
+            //display cmd line argument error
+            catch (ArgumentNullException ane)
+            {
+                outputArea.Insert(0, ane.ToString());
+                drawScreen();
+            }
+            //display socket error
+            catch (SocketException se)
+            {
+                outputArea.Insert(0, se.ToString());
+                drawScreen();
+            }
+            //display expection error
+            catch (Exception e)
+            {
+                outputArea.Insert(0, e.ToString());
+                drawScreen();
+            }
+        }
+    }
+
+
+
+
+    /* ---------------------------
+    *	Name	:   ReadFromServer
+    *
+    *	Purpose :   Thread used to read data from the server continually.  If it is the first receive, 
+    *	            get the servers username for reference in the chat.  if the user enters "*esc", 
+    *	            close communications and close thread
+    *
+    *	Inputs	:	NONE
+    *	
+    *	Outputs	:	Program output is placed into a list and the contents of the list are displayed to the
+    *               screen
+    *               
+    *	Returns	:	NONE
+    */
+    public static void ReadFromServer()
+    {
+        byte[] bytes = new byte[1024];
+        int bytesRec;
+        string data;
+
+        //if first msg received, get the data,
+        //decode and use as the servers username
+        //set firstReceive to false
+        if (firstReceive == true)
+        {
+            bytes = new byte[1024];
+            bytesRec = sender.Receive(bytes);
+            data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+            serverName = data;
+            data = null;
+            firstReceive = false;
+        }
+
+        //constantly read from server
+        while (true)
+        {
+            //Receive the response from the remote device.
+            //if error occurs, display error to user
+            try
+            {
+                //receive and decode and place into data
+                bytesRec = sender.Receive(bytes);
+                outputArea.Insert(0, Encoding.ASCII.GetString(bytes, 0, bytesRec));
+                data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                //id data is the *esc request,
+                //close connections, set firstReceive and FirstSend as true 
+                //and interrupt the write thread to call abort().
+                //exit the application
+                if (data == serverName + ": " + "*esc")
+                {
+                    sender.Close();
+                    firstReceive = true;
+                    firstSend = true;
+                    write.Interrupt();
+                    write.Abort();
+                    Environment.Exit(0);
+                }
+                drawScreen();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+
+
+
+    /* ---------------------------
+    *	Name	:   drawScreen
+    *
+    *	Purpose :   Splits the screen into two sections to display the chat contents and to 
+    *	            show the user the data entered before sending.  This function is adapted from
+    *	            http://stackoverflow.com/questions/3434346/split-a-console-in-two-parts-for-two-outputs
+    *
+    *	Inputs	:	NONE
+    *	
+    *	Outputs	:	Program output is placed into a list and the contents of the list are displayed to the
+    *               screen
+    *               
+    *	Returns	:	NONE
+    */
+    private static void drawScreen()
+    {
+        //if the count of the list is the same as the height of the window,
+        //remove string from specified index
+        if (outputArea.Count == areaHeights)
+        {
+            outputArea.RemoveAt(areaHeights - 1);
+        }
+
+        //flush the stream and clear the cosole
+        Console.Out.Flush();
+        Console.Clear();
+
+        // Draw the area divider "-" along the width of the console
+        for (int i = 0; i < Console.BufferWidth; i++)
+        {
+            Console.SetCursorPosition(i, areaHeights);
+            Console.Write('-');
+        }
+
+        int currentLine = areaHeights - 1;
+
+        //write the data above the divide line
+        for (int i = 0; i < outputArea.Count; i++)
+        {
+            Console.SetCursorPosition(0, currentLine - (i + 1));
+            Console.WriteLine(outputArea[i]);
+        }
+
+        //display the user name, this area is used to show users entered data before
+        //it is sent
+        Console.SetCursorPosition(0, Console.WindowHeight - 1);
+        if (sender != null)
+        {
+            Console.Write(clientName + ": ");
+        }
+    }
+
+
+
+
+    /* ---------------------------
+   *	Name	:   Main
+   *
+   *	Purpose :   Drives the chat program 
+   *
+   *	Inputs	:	Two command line arguments representing the IP address of the host 
+   *                computer to establish a connection and a users desired username.
+   *	
+   *	Outputs	:	Error message if user does not enter a username. Text color is green
+   *	Returns	:	NONE
+   */
+    public static int Main(String[] args)
+    {
+        try
+        {
+            //place commandline arguments into ip address and
+            //username variable respectively.  Change the test color to green
+            string ipaddress = args[0];
+            string userName = args[1];
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            //if the client entered an IP address,
+            //call StartListening method to attempt communications
+            if (ipaddress != null)
+            {
+                StartClient(ipaddress, userName);
+            }
+        }
+        catch (Exception ex)
+        {
+            //error message
+            Console.WriteLine(ex.Message);
+            Console.ReadKey();
+        }
+        return 0;
+    }
+}
---
+++ server/Server.cs
@@ -0,0 +1,344 @@
+﻿//NAME:	        Server.cs
+//PROJECT:      Assignment6
+//DATE:	        04/11/2014
+//AUTHORS:	    Alex Guerrero, Manbir Singh
+//DISCRIPTION:	The server program is used as one side of a chat program that also acts as the host computer for 
+//              communications over a network.  The server program establishes threads to read and write and commands to
+//              exit when the users choose to do so.
+//              HOW TO RUN:  server cmd line arguments required are a chat name, ip address is localhost
+
+
+
+
+//references
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+
+
+
+
+//CLASS
+//-------------------------
+//NAME	:	Server
+//PURPOSE :	This object contains sockets, threads and variables as data members to establish a chat connection over a network
+//-------------------------
+public class Server
+{
+    // Incoming data from the client.
+    public static string data = null;
+    public static Socket listener;
+    public static Socket handler;
+    public static Thread write;
+    public static Thread read;
+    public static string serverName;
+    public static List<string> outputArea = new List<string>();
+    static int areaHeights = 0;
+    static bool firstSend = true;
+    static bool firstReceive = true;
+    static string clientName;
+
+
+
+
+    /* ---------------------------
+    *	Name	:   StartListening
+    *
+    *	Purpose :   The method StartListening() is used to establish a connection to a client on port
+    *               11000 and the host IP address of this computer. The method also starts the read and write threads
+    *               responsible for handling the messaging between the client and server.  If a connection cannot be established 
+    *               a catch will display the error to the user. 
+    *
+    *	Inputs	:	string userName representing the name that will be displayed on the screen for reference.
+    *	
+    *	Outputs	:	NONE
+    *	Returns	:	NONE
+    */
+    public static void StartListening(string userName)
+    {
+        //place the username entered in the command line into serverName
+        serverName = userName;
+
+        //determine the window height  minus 2
+        areaHeights = (Console.WindowHeight - 2);/// 1;
+        drawScreen();
+
+        // Establish the local endpoint for the socket.
+        // Dns.GetHostName returns the name of the 
+        // host running the application.
+        IPHostEntry ipHostInfo = Dns.Resolve("127.0.0.1");
+        IPAddress ipAddress = ipHostInfo.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+        // Create a TCP/IP socket.
+        listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        // Try bind the socket to the local endpoint and 
+        // listen for incoming connections.  Start the read and write threads
+        // and accept the connections.  If an error occurs, 
+        // catch the error and display the error messaeg to the screen.
+        try
+        {
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+
+            read = new Thread(new ThreadStart(ReadFromClient));
+            read.Start();
+            write = new Thread(new ThreadStart(WriteToClient));
+            write.Start();
+        }
+        catch (Exception e)
+        {
+            outputArea.Insert(0, e.ToString());
+            drawScreen();
+        }
+    }
+
+
+
+
+    /* ---------------------------
+    *	Name	:   WriteToClient
+    *
+    *	Purpose :   Thread to write data to client.  The thread accepts user input using 
+    *               Console.ReadLine().  The string is converted to bytes and sent to the client.
+    *               If the server user enters *esc, the server closes the connection and aborts the 
+    *               read tread to terminate the program.
+    *
+    *	Inputs	:	NONE
+    *	
+    *	Outputs	:	NONE
+    *	Returns	:	NONE
+    */
+    public static void WriteToClient()
+    {
+        byte[] msg;
+        int bytesSent;
+
+        //be constantly ready to send a message
+        while (true)
+        {
+            //if a connection is available
+            if (handler != null)
+            {
+                //prepare string to include the servers username, 
+                //then get a line of data from the user
+                data = serverName + ": ";
+                data += Console.ReadLine();
+
+                //when the data is ready and connection is live
+                if (handler != null && data != null)
+                {
+                    //display servers data to the screem
+                    outputArea.Insert(0, data);
+                    drawScreen();
+
+                    //Send the data to the client 
+                    msg = Encoding.ASCII.GetBytes(data);
+                    handler.Send(msg);
+
+                    //if the data to send is the "*esc" to exit the program,
+                    //close the connection, abort the ReadFromClient thread and break;
+                    if (data == serverName + ": " + "*esc")
+                    {
+                        handler.Close();
+                        read.Abort();
+                        break;
+                    }
+                    //clear the buffer
+                    data = null;
+                }
+            }
+        }
+    }
+
+
+
+
+    /* ---------------------------
+    *	Name	:   ReadFromClient
+    *
+    *	Purpose :   Thread used to handle receiving data from client and 
+    *	            displaying the received content to the screen by calling drawScreen().
+    *	            Upon successful connection to a client, the server receives the clients
+    *               username.
+    *               
+    *               When the client sends the *esc to exit the chat, the thread closes the 
+    *               connection and ends the application.
+    *
+    *	Inputs	:	NONE
+    *	
+    *	Outputs	:	NONE
+    *	Returns	:	NONE
+    */
+    public static void ReadFromClient()
+    {
+        byte[] bytes = new Byte[1024];
+
+        while (true)
+        {
+            handler = null;
+            //display message to the screen
+            outputArea.Insert(0, "Waiting for a connection...");
+            drawScreen();
+
+            // Program is suspended while waiting for an incoming connection.
+            //When a connection is made display connection message
+            handler = listener.Accept();
+            outputArea.Insert(0, "Connection has been made, enter *esc to disconnect...");
+            drawScreen();
+
+            //if this is the first receive, get the clients userName and place it into 
+            //a static variable
+            if (firstReceive == true)
+            {
+                //data = serverName;
+                bytes = new byte[1024];
+                //receive data, decode and place into buffer
+                int bytesRec = handler.Receive(bytes);
+                data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                clientName = data;
+
+                //send the servers username and set firstReceieve bool to false
+                //to avoid this section of code for the rest of the program
+                data = serverName;
+                //receive data, encode and place into msg to send
+                byte[] msg = Encoding.ASCII.GetBytes(data);
+                handler.Send(msg);
+                data = null;
+                firstReceive = false;
+            }
+
+
+            //constantly listen for messages
+            while (true)
+            {
+                //process a message, receive data
+                data = null;
+                bytes = new byte[1024];
+                int bytesRec = handler.Receive(bytes);
+                data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                //if the buffer is not empty
+                if (data != "")
+                {
+                    //place data into list and display to screen
+                    outputArea.Insert(0, data);
+                    drawScreen();
+
+                    //the client enters "*esc", close all connections
+                    //display new message to server user set firstReceive to false
+                    //incase need to adjust program for multiple users.
+                    //break from loop
+                    if (data == clientName + ": " + "*esc")
+                    {
+                        handler.Shutdown(SocketShutdown.Both);
+                        handler.Close();
+                        outputArea.Insert(0, "Connection has been terminated");
+                        drawScreen();
+                        //.................
+                        write.Interrupt();
+                        write.Abort();
+                        Environment.Exit(0);
+                        //........................
+                        firstReceive = true;
+                        firstSend = true;
+                        break;
+                    }
+                }
+
+            }
+        }
+    }
+
+
+
+
+    /* ---------------------------
+    *	Name	:   drawScreen
+    *
+    *	Purpose :   Splits the screen into two sections to display the chat contents and to 
+    *	            show the user the data entered before sending.  This function is adapted from
+    *	            http://stackoverflow.com/questions/3434346/split-a-console-in-two-parts-for-two-outputs
+    *
+    *	Inputs	:	NONE
+    *	
+    *	Outputs	:	Program output is placed into a list and the contents of the list are displayed to the
+    *               screen
+     *               
+    *	Returns	:	NONE
+    */
+    private static void drawScreen()
+    {
+        //if the count of the list is the same as the height of the window,
+        //remove string from specified index
+        if (outputArea.Count == areaHeights)
+        {
+            outputArea.RemoveAt(areaHeights - 1);
+        }
+
+        //flush the stream and clear the cosole
+        Console.Out.Flush();
+        Console.Clear();
+
+        // Draw the area divider "-" along the width of the console
+        for (int i = 0; i < Console.BufferWidth; i++)
+        {
+            Console.SetCursorPosition(i, areaHeights);
+            Console.Write('-');
+        }
+
+        int currentLine = areaHeights - 1;
+
+        //write the data above the divide line
+        for (int i = 0; i < outputArea.Count; i++)
+        {
+            Console.SetCursorPosition(0, currentLine - (i + 1));
+            Console.WriteLine(outputArea[i]);
+        }
+
+        //display the user name, this area is used to show users entered data before
+        //it is sent
+        Console.SetCursorPosition(0, Console.WindowHeight - 1);
+        if (handler != null)
+        {
+            Console.Write(serverName + ": ");
+        }
+    }
+
+
+
+
+    /* ---------------------------
+    *	Name	:   Main
+    *
+    *	Purpose :   Drives the chat program 
+    *
+    *	Inputs	:	One command line arguments representing the users username in the program
+    *	
+    *	Outputs	:	Error message if user does not enter a username. Text color is green
+    *	Returns	:	NONE
+    */
+    public static int Main(String[] args)
+    {
+        try
+        {
+            //place command line argument in variable
+            //call startlistening() and pass it the username
+            //change the text color to green
+            string userName = args[0];
+            Console.ForegroundColor = ConsoleColor.Green;
+            StartListening(userName);
+        }
+        catch (Exception ex)
+        {
+            //error message
+            Console.WriteLine(ex.Message);
+            Console.ReadKey();
+        }
+        return 0;
+    }
+}
